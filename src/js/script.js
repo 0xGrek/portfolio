10c336c23fb6d95ab590da1b5736c4768731daf1
Serhii <rfrf198231@gmail.com>
2022-10-05 23:35:37 +0200

    new

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,22 @@
-const   hamburger = document.querySelector ('.hamburger'),
-        menu = document.querySelector('.menu'),
-        closeElem = document.querySelector('.menu__close');
+const hamburger = document.querySelector(".hamburger"),
+    menu = document.querySelector(".menu"),
+    closeElem = document.querySelector(".menu__close"),
+    portfolioItem = document.querySelectorAll(".portfolio__item");
 
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
+hamburger.addEventListener("click", () => {
+    menu.classList.add("active");
 });
 
-closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
+closeElem.addEventListener("click", () => {
+    menu.classList.remove("active");
+});
+portfolioItem.addEventListener("click", () => {
+    portfolioItem.classList.add("active");
 });
 
+const counters = document.querySelectorAll(".skills__raitings-counter"),
+    lines = document.querySelectorAll(".skills__raitings-line span");
 
-const counters = document.querySelectorAll('.skills__raitings-counter'),
-    lines = document.querySelectorAll('.skills__raitings-line span');
-
-counters.forEach( (item, i) => {
-    lines [i].style.width = item.innerHTML;
-
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
 });
-
-
-
-
-
-
