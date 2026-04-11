d5e86f0f982379f54a2b0db73ba864b0183f0f1d
0xGrek <41744903+0xGrek@users.noreply.github.com>
2026-04-11 22:55:19 +0100

    auto: 2026-04-11 22:55 — astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://0xgrek.com',
+  site: 'https://0xgrek.dev',
   integrations: [
     tailwind(),
     sitemap({
